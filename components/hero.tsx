ba98b98e0d88024a20eb3147dff2200622a0a435
Santii <santii075uruguay@gmail.com>
2024-07-12 13:45:29 -0300

    center img

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,6 +7,6 @@ type HeroProps = { searchTerm?: string; totalTitles?: string }
 
 export const Hero = ({ searchTerm, totalTitles }: HeroProps) => (
   <center><div className="container px-4 py-8 mx-auto text-center lg:py-16 lg:px-12 ">
-    <Image src={xataflyWhite} alt="Xata Logo" className="w-6" />
+    <center><Image src={xataflyWhite} alt="Xata Logo" className="w-6" /></center>
   </div></center>
 )
